a92714cd7ca23f84b1add1e6096582fbd00318d9
straycatcoder <straycatcoder@users.noreply.github.com>
2024-01-08 16:13:52 -0500

    enable melpa/try gptel

--- extras/online.el
+++ extras/online.el
@@ -1,9 +1,23 @@
 ;; online packages: AI and Social
 
+;; mastodon social
+;; https://codeberg.org/martianh/mastodon.el
 (use-package mastodon
   :ensure t
   :config
   (setq mastodon-instance-url "https://mastodon.social"
         mastodon-active-user "cyberpoker98"
         mastodon-toot-display-orig-in-reply-buffer t))
+
+;; markdown mode for gptel
+;; https://jblevins.org/projects/markdown-mode/
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown"))
+
+;; gptel
+;; https://github.com/karthink/gptel
+(use-package gptel
+  :ensure t)
 
