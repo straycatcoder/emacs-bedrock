6b163e530b97282786fab1ebdb71fd66947ea015
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2024-01-18 12:35:46 -0500

    display time: HH:MM

--- init.el
+++ init.el
@@ -220,8 +220,8 @@ If the new path's directories does not exist, create them."
 ;; Add the time to the tab-bar, if visible
 (add-to-list 'tab-bar-format 'tab-bar-format-align-right 'append)
 (add-to-list 'tab-bar-format 'tab-bar-format-global 'append)
-(setopt display-time-format "%a %F %T")
-(setopt display-time-interval 1)
+(setopt display-time-format "%a %F %R")
+;(setopt display-time-interval 1)
 (display-time-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
