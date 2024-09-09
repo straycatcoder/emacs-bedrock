2b86d2819648ed4036f15060bcb99bb4d8be6851
straycatcoder <straycatcoder@users.noreply.github.com>
2024-09-09 15:07:02 -0400

    setup hown directory

--- extras/howm.el
+++ extras/howm.el
@@ -1,2 +1,9 @@
 (use-package howm
-  :ensure t)
+  :ensure t
+  :config
+  (setq howm-home-directory "~/Documents/Dropbox/Orgfiles/howm/")
+  (setq howm-directory "~/Documents/Dropbox/Orgfiles/howm/")
+  (setq howm-keyword-file (expand-file-name ".howm-keys" howm-home-directory))
+  (setq howm-history-file (expand-file-name ".howm-history" howm-home-directory))
+  ;(setq howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.md")
+)
